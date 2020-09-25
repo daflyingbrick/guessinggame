321e5e06ee7fd9467413e127b15ceea6e5484c80
daflyingbrick <daflyingbrick@gmail.com>
2020-09-25 10:49:26 -0400

    logic now works

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,14 +1,24 @@
+response=9999
+while [[ $response -ne $listing ]]
+do 
+function get_num_files {
+	listing=$( ls -la | egrep -v '^[d|total]' | wc -l )
+#	echo $listing
+}
+get_num_files
 echo "Enter your guess: How many files in the current director?"
 read response
 echo "You entered: $response"
-function get_num_files {
-	listing=$( ls -la | egrep -v '^[d|total]' | wc -l )
-#	listing=$( ls -la | egrep '^[d]' | wc -l )
-	echo $listing
-}
-get_num_files
-if [[ $response -eq $listing ]]
-then
-	echo "Congratulations, you are correct!"
-fi
-echo "End program"
+
+	if [[ $response -eq $listing ]]
+	then
+		echo "Congratulations, you are correct!"
+#		echo "End program"
+	else
+		if [[ $response -lt $listing ]]
+			then echo "Your guess is low, try again!"
+			else
+				echo "You guess is high, try again!"
+		fi
+	fi
+done
