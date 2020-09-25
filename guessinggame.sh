d6cd207eafeb1fec3ff711652b5479e209c76264
daflyingbrick <daflyingbrick@gmail.com>
2020-09-25 09:19:10 -0400

    got congratulations to work

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,7 +2,13 @@ echo "Enter your guess: How many files in the current director?"
 read response
 echo "You entered: $response"
 function get_num_files {
-	listing=$( ls -l | egrep -v '^[d]' | wc -l )
+	listing=$( ls -la | egrep -v '^[d|total]' | wc -l )
+#	listing=$( ls -la | egrep '^[d]' | wc -l )
 	echo $listing
 }
 get_num_files
+if [[ $response -eq $listing ]]
+then
+	echo "Congratulations, you are correct!"
+fi
+echo "End program"
